89ed6c4814737e9a1b0889af2f9cb83ecf762529
lucasharlaar <lucas.harlaar@gmail.com>
2021-04-11 15:28:15 +0200

    zdf

--- code/Task1/task1b.R
+++ code/Task1/task1b.R
@@ -13,13 +13,25 @@ library(lubridate)
 library(ggplot2)
 library(caret)
 
-ODI = read_csv("./data/ODI/ODI-2021_clean.csv") %>% as_tibble() %>% slice(-1)
+ODI = read.csv("./data/ODI/ODI-2021_clean.csv",header = T, sep =";") %>% as_tibble()
+ODI <- ODI %>% dplyr::mutate(
+  MLCourse=as.factor(MLCourse),
+  IRCourse=as.factor(IRCourse),
+  StatCourse=as.factor(StatCourse),
+  DBCourse=as.factor(DBCourse),
+  Gender=as.factor(Gender),
+  Chocolate=as.factor(Chocolate),
+  Reward=as.numeric(Reward),
+  RandomNo=as.numeric(RandomNo),
+  Bedtime= factor(ODI$Bedtime, levels =c("19","20","21","22","23","0","1","2","3","4","5"),  ordered = F)
+)
 
+# ODI$Bedtime = factor(ODI$Bedtime, levels =c("19","20","21","22","23","0","1","2","3","4","5"),  ordered = T)
 
 
 # Define train control for k fold cross validation
 train_control <- trainControl(method="cv", number=10)
 # Fit Naive Bayes Model
-model <- train(Species~., data=iris, trControl=train_control, method="nb")
+model <- train(finalgd~Gender+Chocolate+Reward+Bedtime, data=ODI, trControl=train_control, method="nb", na.action = na.omit)
 # Summarise Results
 print(model)
