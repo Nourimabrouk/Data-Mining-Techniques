c9701756f0a28a55e937adfe10959bcc90f44f57
lucasharlaar <lucas.harlaar@gmail.com>
2021-04-11 16:09:18 +0200

    GL jayden

--- code/Task1/task1b.R
+++ code/Task1/task1b.R
@@ -26,12 +26,18 @@ ODI <- ODI %>% dplyr::mutate(
   Bedtime= factor(ODI$Bedtime, levels =c("19","20","21","22","23","0","1","2","3","4","5"),  ordered = F)
 )
 
-# ODI$Bedtime = factor(ODI$Bedtime, levels =c("19","20","21","22","23","0","1","2","3","4","5"),  ordered = T)
-
+# Define comparison metric
+metric <- "Accuracy"
 
 # Define train control for k fold cross validation
 train_control <- trainControl(method="cv", number=10)
 # Fit Naive Bayes Model
-model <- train(finalgd~Gender+Chocolate+Reward+Bedtime, data=ODI, trControl=train_control, method="nb", na.action = na.omit)
+set.seed(123)
+model_nb <- train(Gender~Chocolate+Reward+StatCourse, data=ODI, trControl=train_control, method="nb", na.action = na.omit)
+# Fit KNN
+set.seed(123)
+model_knn <- train(Gender~Chocolate+Reward+StatCourse, data=ODI, trControl=train_control, method="knn", na.action = na.omit)
+
 # Summarise Results
-print(model)
+results <- resamples(list(nb=model_nb,knn=model_knn))
+summary(results)
